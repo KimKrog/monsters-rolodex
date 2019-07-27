0e303af75ceb7c6d3a3acb41ce8d6147dec25830
Kim Krog <kimkrog@gmail.com>
2019-07-27 17:46:40 +0200

    Updates

---
+++ precache-manifest.06089c98683a73c34a61d9080531fcf9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e79e4d27da67f13daee44f80ff236a82",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ef42504fe2ec996122a2",
+    "url": "/monsters-rolodex/static/css/main.c1a80d91.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "ef42504fe2ec996122a2",
+    "url": "/monsters-rolodex/static/js/main.93decdd2.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
